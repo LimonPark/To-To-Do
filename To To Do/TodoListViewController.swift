8f8dce33f0b04c7dfd8320aa89b23b594aa5ea2d
nikita <kafes-nik@mail.ru>
2023-09-28 17:30:25 +0300

    Adding persistent local data storage using UserDefaults

--- To To Do/TodoListViewController.swift
+++ To To Do/TodoListViewController.swift
@@ -11,9 +11,13 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = ["Buy milk", "Teach the poem", "Meet with friends"]
 
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK - Tableview Datasource Methods
@@ -53,6 +57,9 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             
             self.tableView.reloadData()
             
